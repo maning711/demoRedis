429375e07981f56a897d532d1ac8ba2b499d3798
maning711 <277943578@qq.com>
2017-03-10 17:05:45 +0800

    maning_集成swagger

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -3,15 +3,16 @@ package com.controller;
 import com.alibaba.fastjson.JSON;
 import com.mapper.UserMapper;
 import com.vo.UserVo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -36,9 +37,17 @@ public class UserController {
     @Resource(name = "stringRedisTemplate")
     ValueOperations<String, String> valueOperationStr;
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @ApiOperation(value="获得用户列表", notes="根据用户id和群组id搜索用户列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "用户ID", required = true, paramType = "query" ,dataType = "String"),
+            @ApiImplicitParam(name = "groupId", value = "群组id", required = true, paramType = "query" ,dataType = "String")
+    })
     @RequestMapping(value = "/users", method = { RequestMethod.GET })
     @ResponseBody
-    public String users(String userId, String groupId) {
+    public String users(@RequestParam String userId, @RequestParam String groupId) {
         Map<String, String> map = new HashMap<String, String>();
         String userVo = "";
         if (userId != null && !userId.isEmpty()) {
@@ -51,27 +60,38 @@ public class UserController {
         // 使用外部缓存
         if (groupId != null && !groupId.isEmpty()) {
 
-            if (valueOperationStr.get(groupId) != null && !valueOperationStr.get(groupId).isEmpty()) {
-
-                userVo = valueOperationStr.get(groupId).toString();
-            } else {
+//            if (valueOperationStr.get(groupId) != null && redisTemplate.hasKey(groupId)) {
+//                userVo = valueOperationStr.get(groupId).toString();
+//            } else {
                 List<UserVo> list = userDao.getUsersList(map);
                 userVo = JSON.toJSONString(list);
                 valueOperationStr.set(groupId, userVo);
-            }
+//            }
         }
         return userVo;
     }
 
-    @RequestMapping("/redis/string/set")
-    public String setKeyAndValue(String key, String value) {
+    @ApiOperation(value="设置redis数据", notes="kv设置")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "key", value = "用户ID", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "value", value = "群组id", required = true, dataType = "String")
+    })
+    @RequestMapping(value="/redis/string/set", method = { RequestMethod.GET })
+    public String setKeyAndValue(
+            @RequestParam String key,
+            @RequestParam String value) {
         logger.debug("访问set:key={},value={}", key, value);
         valueOperationStr.set(key, value);
         return "Set Ok";
     }
 
-    @RequestMapping("/redis/string/get")
-    public String getKey(String key) {
+    @ApiOperation(value="获取redis数据", notes="通过key获取")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "key", value = "数据key", required = true, dataType = "String"),
+    })
+    @RequestMapping(value="/redis/string/get/{key}", method = { RequestMethod.POST })
+    public String getKey(
+            @PathVariable String key) {
         logger.debug("访问get:key={}", key);
         return valueOperationStr.get(key);
     }
